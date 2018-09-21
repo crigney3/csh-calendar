2ea6ccd86cda2bb23d273544fa29680a88f790f6
Corey Rigney <coreyrigney@gmail.com>
2018-09-20 22:09:05 -0400

    first HTML test

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello!"
+    return render_template('home.html')
 
-<html>
-    <head>
-        <title>Bet This wont work</title>
-    </head>
-    <body>
-        <h1>Hello!</h1>
-    </body>
-</html>
 
 if __name__ == "__main__":
     application.run()
