e3bf30f0413a8c29a7c461e61ab3bdf5bb1df534
Corey Rigney <coreyrigney@Coreys-MacBook-Pro.local>
2018-09-21 11:48:22 -0400

    Trying some basic array/variable/random stuff

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import random;
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return render_template('home.html')
 
+@application.route("/array")
+def random():
+    things = [1,2,3,4,5,6,7]
+    placeHolder = random.randint(0,len(things[]));
+    return things[placeHolder]
 
 if __name__ == "__main__":
     application.run()
