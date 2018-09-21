7204d497220bbd49108389e07a212ebba48ecba2
Corey Rigney <coreyrigney@gmail.com>
2018-09-21 11:55:34 -0400

    fix

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import random;
+import Random;
 application = Flask(__name__)
 
 @application.route("/")
@@ -9,7 +9,7 @@ def hello():
 @application.route("/array")
 def random():
     things = [1,2,3,4,5,6,7]
-    placeHolder = random.randint(0,len(things[]));
+    placeHolder = Random.randint(0,len(things));
     return things[placeHolder]
 
 if __name__ == "__main__":
