472a56de850cb51138add0bc765eb75bcd5dbdf3
Corey Rigney <coreyrigney@gmail.com>
2018-09-21 14:21:28 -0400

    app route change

--- wsgi.py
+++ wsgi.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 import random
 application = Flask(__name__)
 
-@application.route("/")
+@approute("/")
 def hello():
     return render_template('home.html')
 
-@application.route("/array")
+@app.route("/array")
 def random():
     things = [1,2,3,4,5,6,7]
     placeHolder = random.randint(0,len(things));
-    return render_template('home.html')
+    return things[placeHolder]
 
 if __name__ == "__main__":
     application.run()
