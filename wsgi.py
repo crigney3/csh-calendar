081dffcab9d83e9d4cea6b323b3aa24d38cbc19b
Corey Rigney <coreyrigney@gmail.com>
2018-09-21 15:08:44 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,7 @@ def hello():
 @application.route("/array")
 def arrayTest():
     things = ["1","2","3","4","5","6","7"]
-    placeHolder = random.randint(0,len(things));
+    placeHolder = random.randint(0,len(things)-1);
     return things[placeHolder]
 
 if __name__ == "__main__":
